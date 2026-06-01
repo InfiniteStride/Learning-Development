a2fc97a0a0097a5637eb56582e22185edbe1c071
InfiniteStride <127489056+InfiniteStride@users.noreply.github.com>
2026-06-01 21:33:17 +0100

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,1 +1,15 @@
+#my bmi calculator if/else statements
+weight = 85
+height = 1.85
 
+bmi = weight / (height ** 2)
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
+
+if bmi <=18.5:
+    print ("underweight")
+elif bmi >= 18.5 and bmi < 25:
+    print("normal weight")
+else:
+    print("overweight")
